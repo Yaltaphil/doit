a6642f315cffd32244563f149917dd7ffca37ae3
Yaltaphil <yaltaphil@yandex.ru>
2021-09-29 13:08:50 +0300

    feature: add adaptiveness

---
+++ mocks/user.js
@@ -0,0 +1,18 @@
+export default {
+    id: 12121212,
+    avatarUrl: '~/assets/img/photo-player.png',
+    name: 'Messi Ronaldo',
+    nickName: 'pele',
+    team: 'Blazer team',
+    withUsFrom: Date.now(),
+    sex: 'male',
+    age: 22,
+    nationality: 'German',
+    country: 'United States',
+    webSite: 'https://doit.gg',
+    games: [
+        { title: 'World of tanks', color: 'red', value: 3 },
+        { title: 'Starcraft', color: 'purple', value: 13 },
+        { title: 'League of Legends', color: 'green', value: 5 },
+    ],
+}
--- pages/userProfile.vue
+++ pages/userProfile.vue
@@ -1,45 +1,135 @@
 <template>
     <div class="page-content">
-        <div class="user-info">
-            <ProfileAvatar>
-                <img src="~/assets/img/photo-player.png" alt="photo" />
-            </ProfileAvatar>
-            name nik team
+        <div class="info">
+            <div class="user-info">
+                <ProfileAvatar>
+                    <img src="~/assets/img/photo-player.png" alt="photo" />
+                </ProfileAvatar>
+                <div class="user-data">
+                    <p class="gray-text">{{ user.name }}</p>
+                    <h3 class="white-text">{{ user.nickName }}</h3>
+                    <nuxt-link to="/"
+                        ><p>
+                            {{ user.team.toUpperCase() }}
+                        </p>
+                    </nuxt-link>
+                    <p>
+                        <nuxt-link to="/">
+                            <img src="~/assets/img/plus-circle.svg" alt="" />
+                        </nuxt-link>
+                        <nuxt-link to="/">
+                            <img src="~/assets/img/mail.svg" alt="" />
+                        </nuxt-link>
+                    </p>
+                </div>
+            </div>
+            <div class="profile-menu">123</div>
         </div>
 
         <div class="profile-table">
             <p class="section-header">Profile</p>
+            <table>
+                <tr>
+                    <td class="gray-text">ID</td>
+                    <td class="white-text">{{ user.id }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Name</td>
+                    <td class="white-text">{{ user.name }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Nickname</td>
+                    <td class="white-text">{{ user.nickName }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">With us from</td>
+                    <td class="white-text">{{ user.withUsFrom }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Sex / Age</td>
+                    <td class="white-text">
+                        {{ user.sex + ' / ' + user.age }}
+                    </td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Nationality</td>
+                    <td class="white-text">{{ user.nationality }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Country</td>
+                    <td class="white-text">{{ user.country }}</td>
+                </tr>
+                <tr>
+                    <td class="gray-text">Web-site</td>
+                    <td class="white-text">
+                        <a class="white-text" :href="user.webSite">{{
+                            user.webSite
+                        }}</a>
+                    </td>
+                </tr>
+            </table>
         </div>
 
         <div class="user-awards">
             <p class="section-header">Level and awards</p>
             <ProfileProgressBar />
+            <ProfileProgressBar />
+            <ProfileProgressBar />
         </div>
     </div>
 </template>
 
+<script>
+import User from '~/mocks/user.js'
+
+export default {
+    data() {
+        return {
+            user: User,
+        }
+    },
+}
+</script>
+
 <style lang="scss" scoped>
 .page-content {
     width: 100%;
+    padding: 1rem;
     display: flex;
     flex-direction: column;
     align-items: center;
+    font-size: 16px;
+    gap: 40px;
+    & .info {
+        // width: 100%;
+    }
     & .user-info {
-        background: green;
-        width: 200px;
-        height: 200px;
         display: flex;
         flex-direction: row;
+        flex-wrap: nowrap;
+        & .user-data {
+            padding: 0 1rem;
+            & img {
+                width: 40px;
+                height: 40px;
+            }
+        }
     }
+    & .profile-menu {
+        display: none;
+    }
+
     & .profile-table {
-        background: purple;
-        width: 200px;
-        height: 200px;
+        & tr {
+            width: 100%;
+            height: 32px;
+            & td {
+                width: 175px;
+            }
+        }
     }
     & .user-awards {
-        background: tomato;
-        width: 200px;
-        height: 200px;
+        // background: tomato;
     }
 }
 
@@ -47,8 +137,24 @@
     .page-content {
         flex-direction: row;
         align-items: flex-start;
+        & .info {
+            min-width: 250px;
+            border-right: 1px solid #1a1f24;
+        }
         & .user-info {
+            max-width: 400px;
             flex-direction: column;
+        }
+        & .profile-menu {
+            background: tomato;
+            display: block;
+            width: 100%;
+        }
+        & .profile-table {
+            max-width: 365px;
+        }
+        & .user-awards {
+            max-width: 408px;
         }
     }
 }
